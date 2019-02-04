7ec248ae9b91dd647741919c00274bf4f1bca37e
tdar31 <t.dardani8@gmail.com>
2019-02-04 03:02:12 -0800

    axios call successfully returns data to frontend

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
-// import axios from "axios";
+import API from "../utils/API";
 // import { Link } from "react-router-dom";
 import SearchBar from "../components/SearchBar";
-import API from "../utils/API";
+import Button from "../components/Button";
 
 class Home extends Component {
   state = {
@@ -15,30 +15,20 @@ class Home extends Component {
     API.getTEST()
       .then(res => console.log("DIDMOUNT: ", res.data))
       .catch(err => console.log(err));
-    // axios
-    //   .get(
-    //     "https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/RiotSchmick?api_key=RGAPI-857f3576-0292-44a1-91f7-23773fea6e35",
-    //     { crossdomain: true }
-    //   )
-    //   .then(res => res.json())
-    //   .then(
-    //     res => {
-    //       console.log("res: ", res);
-    //       // this.setState({
-    //       //   profile: res
-    //       // });
-    //     },
-    //     error => {
-    //       this.setState({
-    //         isLoaded: true,
-    //         error
-    //       });
-    //     }
-    //   );
   }
 
+  handleOnSubmit = event => {
+    event.preventDefault();
+    console.log("Submit button clicked");
+  };
+
   render() {
-    return <SearchBar />;
+    return (
+      <div>
+        <SearchBar />
+        <Button onClick={this.handleOnSubmit}/>
+      </div>
+    );
   }
 }
 
--- controllers/profileController.js
+++ controllers/profileController.js
@@ -3,20 +3,17 @@ var axios = require("axios");
 
 module.exports = {
   findAll: function(req, res) {
-    console.log("FINALL GET TEST");
+    // console.log("FINALL GET TEST");
     axios
       .get(
         "https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/RiotSchmick?api_key=RGAPI-857f3576-0292-44a1-91f7-23773fea6e35"
       )
-      .then(function(res) {
-        console.log("res: ", res.data)
-        // res
-        //   .send(
-        //     "Scrape Complete.  Click back and refresh to see the new articles!"
-        //   )
-        //   .then(dbModel => res.json(dbModel))
-        //   .catch(err => res.status(422).json(err));
-      });
+      .then(res => {
+        console.log("res: ", res.data);
+        return res.data;
+      })
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
   },
   findById: function(req, res) {
     axios
@@ -25,12 +22,7 @@ module.exports = {
       )
       .then(function(res) {
         console.log("res: ", res);
-        // res
-        //   .send(
-        //     "Scrape Complete.  Click back and refresh to see the new articles!"
-        //   )
-        //   .then(dbModel => res.json(dbModel))
-        //   .catch(err => res.status(422).json(err));
+        return res.data
       });
   }
   // create: function(req, res) {
