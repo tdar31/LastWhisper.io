8ae6f762b9f22dee4d9d1f8aa9c2d12cb831be43
tdar31 <t.dardani8@gmail.com>
2019-02-12 02:54:13 -0800

    building out tooltips and game module

--- client/src/components/GameModuleChampInfo/index.js
+++ client/src/components/GameModuleChampInfo/index.js
@@ -1,15 +1,16 @@
 import React from "react";
+import ReactTooltip from "react-tooltip";
 import "./style.css";
 
 function GameModuleChampInfo({ champName, champIcon, spell1, spell2, role }) {
   return (
     <div className="GameModuleChampInfo">
+      <ReactTooltip place="top" type="dark" effect="solid"/>
       <div className="champContainer">
         <img alt="img" className="champIcon" src={champIcon} />
-        <img alt="img" className="spell1" src={spell1} />
-        <img alt="img" className="spell2" src={spell2} />
+        <img alt="img" className="spell1" data-tip="Ignite" src={spell1} />
+        <img alt="img" className="spell2" data-tip="Flash" src={spell2} />
       </div>
-
       <div className="champName">{champName}</div>
       <div className="role">{role}</div>
     </div>
