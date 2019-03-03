5afe9c0d07e6c746882248ef1a2cb4497ed24124
tdar31 <t.dardani8@gmail.com>
2019-03-03 10:50:47 -0800

    notifications setup

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,10 +1,15 @@
 import React, { Component } from "react";
 import { BrowserRouter as Link } from "react-router-dom";
+import {
+  NotificationContainer,
+  NotificationManager
+} from "react-notifications";
 import HomeContainer from "../components/HomeContainer";
 import HomeBody from "../components/HomeBody";
 import Nav from "../components/Nav";
 import SearchBar from "../components/SearchBar";
 import Banner from "../components/Banner";
+import "react-notifications/lib/notifications.css";
 
 class Home extends Component {
   state = {
@@ -18,6 +23,7 @@ class Home extends Component {
 
   componentDidMount() {
     this.setSelectedButton = this.setSelectedButton.bind(this);
+    NotificationManager.info("", "Close after 3000msClose after 3000msClose after 3000msClose after 3000msClose after 3000msClose after 3000msClose after 3000ms", 30000);
   }
 
   toggle() {
@@ -59,12 +65,66 @@ class Home extends Component {
     });
   }
 
+  createNotification = () => {
+    // return () => {
+    //   switch (type) {
+    //     case "info":
+    //       NotificationManager.info("Info message");
+    //       break;
+    //     case "success":
+    //       NotificationManager.success("Success message", "Title here");
+    //       break;
+    //     case "warning":
+    //       NotificationManager.warning(
+    //         "Warning message",
+    //         "Close after 3000ms",
+    //         3000
+    //       );
+    //       break;
+    //     case "error":
+    //       NotificationManager.error("", "Click me!", 5000, () => {
+    //         alert("callback");
+    //       });
+    //       break;
+    //   }
+    // };
+  };
+
   render() {
     return (
       <HomeContainer>
         <Nav />
         <HomeBody>
+          <div>
+            <div className="statusNotification"
+              onClick={this.createNotification()}
+            />
+            {/* <hr />
+            <button
+              className="btn btn-success"
+              onClick={this.createNotification("success")}
+            >
+              Success
+            </button>
+            <hr />
+            <button
+              className="btn btn-warning"
+              onClick={this.createNotification("warning")}
+            >
+              Warning
+            </button>
+            <hr />
+            <button
+              className="btn btn-danger"
+              onClick={this.createNotification("error")}
+            >
+              Error
+            </button> */}
+
+            <NotificationContainer />
+          </div>
           <Banner />
+          <p className="suggest"><i>Try Meteos, Goldenglue or Doublelift</i></p>
           <SearchBar
             onChange={this.handleInputChange}
             onClick={this.handleOnSubmit}
